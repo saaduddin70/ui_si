1d0cfa0e27f4bcc60dc4247785526bf9dcf1de85
Saad <uddinsaad4567@gmail.com>
2024-04-18 09:34:08 +0600

    f

--- app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.java
@@ -1,2 +1,27 @@
-package com.example.myapplication;public class MainActivityTest {
+package com.example.myapplication;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.assertion.ViewAssertions;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testButtonClick_Red() {
+        // Click the button1
+        Espresso.onView(ViewMatchers.withId(R.id.button1)).perform(ViewActions.click());
+
+        // Check if the text view displays "One"
+        Espresso.onView(ViewMatchers.withId(R.id.textView2))
+                .check(ViewAssertions.matches(ViewMatchers.withText("one")));
+    }
 }
